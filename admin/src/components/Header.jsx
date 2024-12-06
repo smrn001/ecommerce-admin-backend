d92b8108f3721776b304181b9e63a614a7e1c0f6
smrn001 <samir.niroula001@gmail.com>
2024-12-06 12:54:52 +0545

    ..

--- admin/src/components/Header.jsx
+++ admin/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Menu, CircleUserRound } from "lucide-react";
 
 const Header = ({ toggleSidebar }) => {
   return (
-    <nav className="bg-gray-800 fixed w-full  top-0 flex items-center p-4 shadow-md gap-3 z-10 justify-between md:px-24">
+    <nav className="bg-gray-800 fixed w-full max-w-[1240px] top-0 flex items-center p-4 shadow-md gap-3 z-10 justify-between md:px-24">
       <button
         onClick={toggleSidebar}
         aria-label="Toggle Sidebar"
